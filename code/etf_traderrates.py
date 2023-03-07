f340b0ef6539ab4bc02d5b49a3bb4491b17b386c
Marius Zoican <m.a.zoican@gmail.com>
2023-03-07 17:57:44 -0500

    Update ETF traders & duration

--- code/etf_traderrates.py
+++ code/etf_traderrates.py
@@ -32,62 +32,92 @@ def settings_plot(ax):
     return ax
 
 # load manager panel
+# --------------------------
 manager_data=pd.read_csv("../data/manager_panel.csv.gz",index_col=0)
-# load ETF panel
-etf_panel=pd.read_csv("../data/etf_panel_raw.csv")
+# Follow Broman-Shum (2018) and keep only quasi-indexers and transient investors
 
+# load ETF panel
+# -----------------------------
+etf_panel=pd.read_csv("../data/etf_panel_raw.csv")
+# keep ETFs with at least 10 quarters of data, and exclude the first 2 quarters of an ETF existence
+# (Broman-Shum, 2018)
+etf_panel['inception']=etf_panel['inception'].apply(lambda x: dt.datetime.strptime(x, "%d/%m/%Y"))
+etf_panel['inception_q']=etf_panel['inception'].dt.year*10+etf_panel['inception'].dt.quarter
+etf_panel['quarter_decimal']=etf_panel['quarter'].apply(lambda x: int(x/10)+(x%10-1)/4)
+etf_panel['inc_quarter_decimal']=etf_panel['inception_q'].apply(lambda x: int(x/10)+(x%10-1)/4)
+etf_panel['time_existence']=etf_panel['quarter_decimal']-etf_panel['inc_quarter_decimal']
+etf_panel=etf_panel[etf_panel.time_existence>0.5] # exclude first 1/2 years of existence
+etf_panel['quarters_in_sample']=etf_panel.groupby('ticker')['quarter'].transform('count')
+etf_panel=etf_panel[etf_panel.quarters_in_sample>=10] # keep ETFs with at least 10 quarters in sample
+
+# keep the top 2 ETFs by AUM in each index-quarter
 def rank_group(df,k,in_column,out_column):
     df[out_column] = df[in_column].rank(method='dense', ascending=False) <= k
     return df
-
-# keep the top 2 ETFs by AUM in each index-quarter
 etf_panel=etf_panel.groupby(['index_id','quarter']).apply(lambda x: rank_group(x,2,'aum','top2aum'))
 etf_panel=etf_panel[etf_panel.top2aum]
 del etf_panel['top2aum']
 
+
 # list of ETF tickers
 list_ETF_tickers=etf_panel.ticker.drop_duplicates().tolist()
-
 etf_panel['etf_per_index']=etf_panel.groupby(['index_id','quarter'])['ticker'].transform('count')
 
 # label the high-fee ETF in each index-quarter
-
 etf_panel['uniquevals']=etf_panel.groupby(['index_id','quarter'])['mer_bps'].transform('nunique')
 etf_panel['rank_fee']=etf_panel.groupby(['index_id','quarter'])['mer_bps'].rank(method='dense')
-
 etf_panel['rank_fee']=np.where(etf_panel['uniquevals']==2, etf_panel['rank_fee'],np.nan)
-
-
-etf_panel['highfee']=np.where(etf_panel['rank_fee']==1,1,
-                                np.where(etf_panel['rank_fee']==2,0,np.nan))
-
-
-
-# etf_panel=etf_panel.groupby(['index_id','quarter']).apply(lambda x: rank_group(x,1,'mer_bps','highfee'))
+etf_panel['highfee']=np.where(etf_panel['rank_fee']==2,1,
+                                np.where(etf_panel['rank_fee']==1,0,np.nan))
 etf_panel['highfee']=np.where(etf_panel['etf_per_index']==2, 1*etf_panel['highfee'], np.nan)
+# etf_panel=etf_panel[etf_panel.us_index==1]
 
-# load 13F data for the ETF holdings in our sample
-data13F=pd.read_csv("../data/data_13F_ETFonly_RR.csv.gz",index_col=0)
+index_us=pd.read_csv("../data/indices_uslabel.csv")
+etf_panel=etf_panel.merge(index_us,on='index_id',how='left') # dummy is index is US-focused
 
-# merge 13F with manager data
-data13F['year']=data13F['rdate'].apply(lambda x: float(x[0:4]))
-cols_mgr=['mgrno','year','lambda_manager','mgr_duration','type','transient_investor','tax_extend']
-data13F=data13F.merge(manager_data[cols_mgr],on=['mgrno','year'],how='left')
+# load 13-F based duration measure (Cremers and Pareek, 2016)
+# -------------------------------------------------------------------------
+d13furg=pd.read_csv("../data/duration_13F.csv.gz",index_col=0)
+d13furg=d13furg[d13furg.ticker.isin(list_ETF_tickers)] 
+d13furg=d13furg.rename(columns={'duration':'mgr_duration'})
 
-# compute ETF-level weighted average trading urgency
-etf_intensity = data13F.groupby(['ticker',
-                                'quarter']).apply(lambda x: 
-                                 (x['lambda_manager']*x['shares']).sum()/x['shares'].sum()).reset_index()
-etf_intensity=etf_intensity.rename(columns={0:'mgr_intensity'})
+cols_mgr=['mgrno','quarter','horizon_perma','type','tax_extend']
+d13furg=d13furg.merge(manager_data[cols_mgr],on=['mgrno','quarter'],how='left')
+d13furg=d13furg[d13furg.horizon_perma.isin(['QIX','TRA'])]
 
-# compute ETF-level weighted average duration
-etf_duration = data13F.groupby(['ticker',
+
+unique_mgr_qtr=d13furg.drop_duplicates(subset=['mgrno','quarter'])
+unique_mgr_qtr['quarter_count']=unique_mgr_qtr.groupby(['mgrno']).cumcount()
+unique_mgr_qtr=unique_mgr_qtr[['mgrno','quarter','quarter_count']]
+
+d13furg=d13furg.merge(unique_mgr_qtr, on=['mgrno','quarter'],how='left')
+d13furg['quarter_count']=d13furg['quarter_count'].fillna(0)
+d13furg=d13furg[d13furg.quarter_count>=8]
+
+d13furg['quarter_decimal']=d13furg['quarter'].apply(lambda x: int(x/10)+(x%10-1)/4)
+# first quarter of investment for that investor
+d13furg['first_quarter_inv']=d13furg.groupby(['mgrno','ticker'])['quarter_decimal'].transform('first')
+d13furg['time_since_first_inv']=d13furg['quarter_decimal']-d13furg['first_quarter_inv']
+
+etf_duration = d13furg.groupby(['ticker',
                                 'quarter']).apply(lambda x: 
                                  (x['mgr_duration']*x['shares']).sum()/x['shares'].sum()).reset_index()
 etf_duration=etf_duration.rename(columns={0:'mgr_duration'})
 
+etf_time_since_first = d13furg.groupby(['ticker',
+                                'quarter']).apply(lambda x: 
+                                 (x['time_since_first_inv']*x['shares']).sum()/x['shares'].sum()).reset_index()
+etf_time_since_first=etf_time_since_first.rename(columns={0:'time_since_first'})
+
+etf_duration=etf_duration.merge(etf_time_since_first,on=['ticker','quarter'],how='left')
+
+
+
+
+
 # compute share of AUM held by tax-insensitive investors (TII)
-tax_sensitivity=data13F.groupby(['ticker','quarter','tax_extend']).agg({'shares':sum,'shrout2':np.mean}).reset_index()
+tax_sensitivity=d13furg.groupby(['ticker','quarter',
+                                 'tax_extend']).agg({'shares':sum}).reset_index()
 tax_sensitivity['total_shares_sample']=tax_sensitivity.groupby(['ticker','quarter',
                                                                 ])['shares'].transform(sum)
 tax_sensitivity['ratio_tii']=tax_sensitivity['shares']/tax_sensitivity['total_shares_sample']*100
@@ -95,29 +125,14 @@ tax_sensitivity=tax_sensitivity[tax_sensitivity.tax_extend=='TII']
 tax_sensitivity=tax_sensitivity[['ticker','quarter','ratio_tii']]
 
 # compute share of AUM held by transient investors (according to Bushee classification)
-# transient=data13F.groupby(['ticker','quarter','transient_investor']).agg({'shares':sum,'shrout2':np.mean}).reset_index()
-# transient['total_shares_sample']=transient.groupby(['ticker','quarter',
-#                                                                 ])['shares'].transform(sum)
-# transient['ratio_tra']=transient['shares']/(transient['total_shares_sample'])
-# transient=transient[transient.transient_investor=='TRA']
-# transient=transient[['ticker','quarter','ratio_tra']]
-
-# transient=data13F.groupby(['ticker','quarter','transient_investor']).count()['rdate'].reset_index()
-# transient['total_investors']=transient.groupby(['ticker','quarter'])['rdate'].transform(sum)
-# transient['ratio_tra']=transient['rdate']/transient['total_investors']
-# transient=transient[transient.transient_investor=='TRA']
-# transient=transient[['ticker','quarter','ratio_tra']]
-
-transient=data13F.groupby(['ticker','quarter','transient_investor']).agg({'shares':sum,'shrout2':np.mean}).reset_index()
-transient=transient.merge(etf_panel[['ticker','index_id']].drop_duplicates(),on='ticker',how='left')
-transient=transient[transient.transient_investor=='TRA']
-transient['index_shares']=transient.groupby(['index_id','quarter'])['shares'].transform(sum)
-transient['ratio_tra']=transient['shares']/transient['index_shares']
+transient=d13furg.groupby(['ticker','quarter','horizon_perma']).agg({'shares':sum}).reset_index()
+transient['total_shares_sample']=transient.groupby(['ticker','quarter',
+                                                                ])['shares'].transform(sum)
+transient['ratio_tra']=transient['shares']/(transient['total_shares_sample'])
+transient=transient[transient.horizon_perma=='TRA']
 transient=transient[['ticker','quarter','ratio_tra']]
 
-etf_measures=etf_duration.merge(etf_intensity,on=['ticker','quarter'],how='outer')
-
-etf_measures=etf_measures.merge(tax_sensitivity,on=['ticker','quarter'],
+etf_measures=etf_duration.merge(tax_sensitivity,on=['ticker','quarter'],
                                  how='outer').merge(transient,on=['ticker','quarter'],how='outer')
 etf_panel=etf_panel.merge(etf_measures,on=['ticker','quarter'],how='left')
 
@@ -136,73 +151,33 @@ etf_panel['stock_tweets']=etf_panel['stock_tweets'].fillna(0)
 etf_panel['stock_tweets']=etf_panel['stock_tweets'].apply(lambda x: np.log(1+x))
 
 etf_panel['qduration']=pd.qcut(etf_panel['mgr_duration'], q=5, labels=False)+1
-etf_panel['qintensity']=pd.qcut(etf_panel['mgr_intensity'], q=5, labels=False)+1
 
-etf_panel.to_csv("../data/etf_panel_processed.csv")
+etf_graph=etf_panel[(etf_panel.etf_per_index==2)].dropna(subset=['highfee']).copy()
 
-# get some plots on urgency and ETF Fee
-sizefigs_L=(20,8)
-fig=plt.figure(facecolor='white',figsize=sizefigs_L)
-gs = gridspec.GridSpec(1, 3)
+etf_graph.to_csv("../data/etf_panel_processed.csv")
 
-ax=fig.add_subplot(gs[0, 0])
-ax=settings_plot(ax)
-# sns.kdeplot(data=etf_panel[(etf_panel.etf_per_index==2)].dropna(subset=['highfee']), 
-#              x='mgr_duration', hue='highfee',common_norm=False)
-sns.barplot(data=etf_panel[(etf_panel.etf_per_index==2)].dropna(subset=['highfee']), 
-            x='qduration', y='highfee', palette='mako',capsize=0.1)
-ax.set_xticklabels(['Low','Q2','Q3','Q4','High'],fontsize=18)
-plt.ylabel('Fraction of high-fee ETFs',fontsize=18)
-plt.xlabel('Investor stock duration quantile',fontsize=18)
-
-# handles = [mpatches.Patch(facecolor=plt.cm.Reds(100), label="High-fee ETF"),
-#            mpatches.Patch(facecolor=plt.cm.Blues(100), label="Low-fee ETF")]
-# plt.legend(handles=handles,fontsize=18,frameon=False,loc='upper right')
-
-ax=fig.add_subplot(gs[0, 1])
-ax=settings_plot(ax)
-sns.kdeplot(data=etf_panel[(etf_panel.etf_per_index==2)].dropna(subset=['highfee']), 
-             x='mgr_intensity', hue='highfee',common_norm=False)
-plt.ylabel('Probability density',fontsize=18)
-plt.xlabel('Investor trading intensity',fontsize=18)
-
-handles = [mpatches.Patch(facecolor=plt.cm.Reds(100), label="High-fee ETF"),
-           mpatches.Patch(facecolor=plt.cm.Blues(100), label="Low-fee ETF")]
-plt.legend(handles=handles,fontsize=18,frameon=False)
-
-
-ax=fig.add_subplot(gs[0, 2])
-ax=settings_plot(ax)
-sns.kdeplot(data=etf_panel[(etf_panel.etf_per_index==2)].dropna(subset=['highfee']), 
-             x='ratio_tra', hue='highfee',common_norm=False)
-plt.ylabel('Probability density',fontsize=18)
-plt.xlabel('Share of transient investors',fontsize=18)
-
-handles = [mpatches.Patch(facecolor=plt.cm.Reds(100), label="High-fee ETF"),
-           mpatches.Patch(facecolor=plt.cm.Blues(100), label="Low-fee ETF")]
-plt.legend(handles=handles,fontsize=18,frameon=False)
-
-plt.tight_layout(pad=2)
-plt.savefig("../output/liquidityneed_by_mer.png",bbox_inches="tight")
-
-
-# stockmean=panel.copy()
-# stockmean=stockmean.set_index(['gic','YYYYQ'])
-
-# full_rv= PanelOLS.from_formula('RetailValue~1+lnMCAP+TURN+IO+bm_ratio + rd + age + RET + \
-#                             absRET + Volatility+ EntityEffects', data=stockmean).fit()
-# full_ac= PanelOLS.from_formula('numest~1+lnMCAP+TURN+IO+bm_ratio + rd + age + RET + \
-#                             absRET + Volatility+EntityEffects', data=stockmean).fit()
-# stockmean['ResRetailValue'] = full_rv.resids
-# stockmean['ResNumest'] = full_ac.resids
 
 from linearmodels.panel import PanelOLS
-etf_graph=etf_panel[(etf_panel.etf_per_index==2)].dropna(subset=['highfee']).copy()
 etf_graph=etf_graph.set_index(['index_id','quarter'])
 etf_graph=etf_graph.dropna()
-inv_dur_reg=PanelOLS.from_formula('mgr_duration~1 + EntityEffects + TimeEffects',data=etf_graph).fit()
-inv_int_reg=PanelOLS.from_formula('mgr_intensity~1 + EntityEffects + TimeEffects',data=etf_graph).fit()
+inv_dur_reg=PanelOLS.from_formula('mgr_duration~1 + EntityEffects + TimeEffects + time_existence + time_since_first ',data=etf_graph).fit()
 etf_graph['dur_resid']=inv_dur_reg.resids
-etf_graph['int_resid']=inv_int_reg.resids
 etf_graph['qduration']=pd.qcut(etf_graph['dur_resid'], q=5, labels=False)+1
-etf_graph['qintensity']=pd.qcut(etf_graph['int_resid'], q=5, labels=False)+1
+
+
+d13furg=d13furg.merge(etf_graph.reset_index()[['ticker',
+                                                     'quarter','highfee']],on=['ticker','quarter'],how='left')
+
+
+test=etf_graph.reset_index().pivot(index=['index_id','quarter'],columns=['highfee'],
+                                   values=['mer_bps','mgr_duration','ratio_tra'])
+test['mer_diff']=test[ (      'mer_bps', 1.0)]-test[ (      'mer_bps', 0.0)]
+test['mgr_duration_diff']=test[ (      'mgr_duration', 1.0)]-test[ (      'mgr_duration', 0.0)]
+test['ratio_tra_diff']=test[ (      'ratio_tra', 1.0)]-test[ (      'ratio_tra', 0.0)]
+
+test['mer_avg']=0.5*(test[ (      'mer_bps', 1.0)]+test[ (      'mer_bps', 0.0)])
+test['mgr_duration_avg']=0.5*(test[ (      'mgr_duration', 1.0)]+test[ (      'mgr_duration', 0.0)])
+test['ratio_tra_avg']=0.5*(test[ (      'ratio_tra', 1.0)]+test[ (      'ratio_tra', 0.0)])
+
+test['mgr_duration_rel_diff']=test['mgr_duration_diff']/test['mgr_duration_avg']
+test['ratio_tra_rel_diff']=test['ratio_tra_diff']/test['ratio_tra_avg']
--- regression_code_RR/regs.do
+++ regression_code_RR/regs.do
@@ -2,4 +2,4 @@
 import delimited "D:\Research\kpz_etfliquidity\data\etf_panel_processed.csv", clear 
 
 reghdfe mgr_duration highfee ratio_tii stock_tweets marketing_fee_bps  , absorb(index quarter) vce(cl index quarter)
-reghdfe mgr_intensity  highfee ratio_tii stock_tweets marketing_fee_bps  , absorb(index quarter) vce(cl index quarter)
+reghdfe mgr_intensity highfee ratio_tii stock_tweets marketing_fee_bps  , absorb(index quarter) vce(cl index quarter)
